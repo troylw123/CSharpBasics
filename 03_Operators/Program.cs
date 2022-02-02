8ef04191eba1ae66eea7c046970437ec9247734a
Troy Weaver <troylw@gmail.com>
2022-02-02 12:06:05 -0500

    Adding 03_Operators to the Fundamentals solution/project

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 = " + additionResult);
+
+int subtractionResult = 5 - 2;
+Console.WriteLine("5 - 2 = " + subtractionResult);
+
+int multiplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+decimal divisionResult = 5 / 2m;
+Console.WriteLine("5 / 2 = " + divisionResult);
+
+double divisionResult2 = 5 / 2d;
+Console.WriteLine("5 / 2 = " + divisionResult2);
+
+// % is called modulus. It divides the numbers and gives you the remainder.
+Console.WriteLine(10 % 2);
+Console.WriteLine(10 % 4);
+Console.WriteLine(16 % 3);
+
+
