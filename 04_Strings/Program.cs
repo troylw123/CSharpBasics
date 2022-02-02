4c267068f5fc16361d35a8e6dcff6b6f7f861f55
Troy Weaver <troylw@gmail.com>
2022-02-02 12:24:39 -0500

    Adding 04_Strings to Fundamentals solution

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexus, Mercedes, and Tesla.";
+
+// String Concatenation
+string concatenatedResult = first + " " + second;
+Console.WriteLine(concatenatedResult);
+
+
+string firstName = "Jenn";
+string lastName = "Williams";
+
+// Composite Formatting
+string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+Console.WriteLine(compositeResult);
+Console.WriteLine("Her name is {0} {1}.", firstName, lastName);
+
+// String Interpolation
+string interpolationResult = $"Her name is still {firstName} {lastName}!";
+Console.WriteLine(interpolationResult);
+Console.WriteLine($"Her name is still {firstName} {lastName}!");
+
