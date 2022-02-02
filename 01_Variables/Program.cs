9b16a50430701e37ce4ec965141a03337b39e219
Troy Weaver <troylw@gmail.com>
2022-02-02 10:47:47 -0500

    adding 01_Variables to full solution/project

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+
+Console.WriteLine("What is your favorite color?");
+string color = Console.ReadLine();
+
+Console.WriteLine("You said your favorite color is " + color + ".");
+
