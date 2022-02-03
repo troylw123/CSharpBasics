95275f0903fae19259269c2497e0731eff7173d5
Troy Weaver <troylw@gmail.com>
2022-02-03 10:32:10 -0500

    Adding 06_Ternaries inside 06_Conditionals

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,11 @@
+﻿// Write doesn't have a line break after it like WriteLine does
+Console.Write("Enter your age:");
+string response = System.Console.ReadLine();
+// Parse changes type to int, if possible
+int age = int.Parse(response);
+
+string output = age >= 18 ? "You can vote!" : "You're too young to vote.";
+
+Console.WriteLine(output);
+
+
